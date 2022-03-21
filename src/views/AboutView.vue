560914dd6345eea04e80d2403d1ec3437a0d6d8b
ehab-fadl <56725108+ehab-fadl@users.noreply.github.com>
2022-03-21 22:47:32 +0100

    ehab-fadl
    first app

---
+++ src/views/AboutView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="about">
+    <h1 class="h1Style">Welcome to Vue.js page</h1>
+
+      <table class="table table-hover table-dark">
+  <thead>
+    <tr>
+      <th>Title</th>
+      <th>Author</th>
+      <th>Number of Pages</th>
+      <th>Publish date</th>
+       <th>Download</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="item in list" :key="item.id">
+      <th>{{item.Tirle}}</th>
+      <td>{{item.Author}}</td>
+      <td>{{item.NumberOfPages}}</td>
+      <td>{{item.PublishesAt}}</td>
+       <td><a v-bind:href="item.Link"><button class="btn btn-info">Download</button></a></td>
+    </tr>
+    
+  </tbody>
+</table>
+
+
+  </div>
+</template>
+
+
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios,axios);
+
+export default {
+  name: 'AboutView',
+  components: {
+   
+  },
+  data(){
+    return {list:undefined}
+  },
+  //THB//http://192.168.0.100:6555/api/books
+  mounted(){
+    Vue.axios.get('http://192.168.0.10:8888/api/books').then(resp=>{
+     this.list=resp.data;
+      
+    })
+  }
+  
+}
+</script>
+
+<style>
+.h1Style{
+
+  color:darkorange;
+  font-size: 70px;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1 class="h1style">Welcome to Vue.js Page</h1>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+   
+  }
+}
+</script>
+
+
+<style>
+.h1style{
+  color: darkseagreen;
+  font-size: 70px;
+}
+
+</style>
